951884aa257767da42f513d8ab988ebda3805c4b
Serhii Sosnytskyi <55388412+AlexUnderNewSky@users.noreply.github.com>
2024-02-16 14:08:49 +0200

    Add project files.

---
+++ Lessons/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+
+
+namespace Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "Hello World!";
+            Console.WriteLine(a + "FUCKING WORLD");
+
+            Console.WriteLine("Привет ЁБАННЫЙ МИР!!!");
+        }
+    }
+}
