13bd4025af4242670755691483462e8cd25f3df6
Serhii Sosnytskyi <55388412+AlexUnderNewSky@users.noreply.github.com>
2024-02-16 16:24:52 +0200

    Lesson4Done

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -12,10 +12,88 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            String a = "Hello World!";
-            Console.WriteLine(a + "FUCKING WORLD");
+            
 
-            Console.WriteLine("Привет ЁБАННЫЙ МИР!!!");
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*String a = "Hello World!";
+Console.WriteLine(a + "FUCKING WORLD");
+
+Console.WriteLine("Привет ЁБАННЫЙ МИР!!!");
+Console.WriteLine("Please, enter a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, enter b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+=====================================================================
+
+if (a >= 1 && a <= 10)
+{
+    a = 15;
+}
+else
+{
+    Console.WriteLine("A is not empty!");
+}
+
+Console.WriteLine("a = " + a + " b = " + b);
+
+=====================================================================
+ 
+            bool variableFalse = false;
+
+            bool variableTrue = true;
+
+            Console.WriteLine(variableFalse);
+
+            Console.WriteLine(variableTrue);
+
+=====================================================================
+ 
+ Console.WriteLine("Please, enter b: ");
+            int b = Convert.ToInt16(Console.ReadLine());
+            int a = 0;
+            if (b > 10)
+            {
+                a = 1;
+            }
+            else { a = 5; }
+
+            Console.WriteLine(a);
+
+            a = 55;
+            Console.WriteLine(a);
+ */
+
+
