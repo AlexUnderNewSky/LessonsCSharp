40ecfd46430fff6309ab5d6f47f08b589dcc30e2
Serhii Sosnytskyi <55388412+AlexUnderNewSky@users.noreply.github.com>
2024-02-26 12:59:27 +0200

    Lesson 32 done!

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -23,7 +23,7 @@ namespace Lessons
         {
             //new Homework().HomeworkCsharp();
 
-            
+           
             Console.ReadLine();
         }
 
@@ -613,6 +613,36 @@ Example 1
                 Console.Write($"{item} ");
             }
 
+=====================================================================
+ LESSON 32!!!!!!!!!!!!!!!!!!!
+
+
+            string[,] myArray = new string[2, 3];
+            //Random random = new Random();
+
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Please enter Y: {i} and X: {j} value");
+                    myArray[i, j] = Console.ReadLine();
+
+                }
+                
+            }
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write($"{myArray[y, x]} \t");
+                }
+                Console.WriteLine();
+            }
+
+          
+            Console.ReadLine();
 */
 
 
